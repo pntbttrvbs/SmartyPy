948b7d070ed3c7964722d847f086de43e6ef693e
jspott <jspott@kmlabs.com>
2019-05-13 18:16:44 -0600

    started building basic test for API

---
+++ tests.py
@@ -0,0 +1,26 @@
+import SmarAct
+import unittest
+
+class movementTests(unittest.TestCase):
+
+    def __init__(self):
+        super.__init__()
+        self.sampleStage_IP = 192.168.1.201
+        self.focusStage_IP = 192.168.1.200
+        self.port = 5000
+
+
+    def setUp(self):
+        self.SA = SmarAct.SmarAct()
+        print(self.SA.GetDLLVersion())
+
+    def testInitConnection(self):
+        input("Connect ethernet to leftmost controller and press enter")
+        
+    def tearDown(self):
+        pass
+        #self.SA.CloseSystem()
+
+
+if __name__ == '__main__':
+    unittest.main()
